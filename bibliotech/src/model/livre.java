493b05478140a4709c1321b2378e6860a412e072
Nilton Mana <niltonmana96@gmail.com>
2026-02-05 20:41:14 +0100

    Ajout du modèle

---
+++ bibliotech/src/model/livre.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Génère Getters, Setters, toString, equals, hashCode
+@NoArgsConstructor // Génère un constructeur vide (obligatoire pour beaucoup de frameworks)
+@AllArgsConstructor // Génère un constructeur avec tout les champs
+
+public class livre {
+	private Long id;
+	private String auteur;
+	private String isbn;
+	private String categorie;
+	private int anneeParution;
+	private boolean disponible;
+}
